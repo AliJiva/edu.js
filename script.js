28690fac211e35017df35ae3238a589443ecd24f
Сейткалиев Алибек Кенесович <aseitkaliyev@kazatomprom.kz>
2020-03-06 19:18:22 +0600

    changed

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let number = 5;
+let string = 'hello';
+let sym = Symbol();
+let boolean = true;
+null;
+undefined;
+
+let obj = {
+    name: 'Ali',
+    age: 18,
+    status: 'Single'
+}
+
+// console.log(obj['name']);
+
+// let arr = [1, 'eeee', 123, null];
+
+// console.log(typeof 'arr' + ' - object');
+// console.log(4 + +' - 5');
+
+// let str = "10",
+//     num = 10;
+
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(str ==  num);
+// console.log(str ===  num);
+// console.log(str + num);
+
+let isChecked = true,
+    isClose = true;
+
+    console.log(true || true);
+
+
+
